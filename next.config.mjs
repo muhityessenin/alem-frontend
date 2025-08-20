e4c0987a83d194fbcaf2a2f2f0a7e4a479d5779d
muhitesenin <eseninmukhit@gmail.com>
2025-08-20 16:39:53 +0500

    origins configured

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    allowedDevOrigins: ['*'],  // Replace with your local IP address
+  },
 }
 
 export default nextConfig
